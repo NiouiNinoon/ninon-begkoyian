351fc7f5f5fd3ad45a83f76d54790177f238a761
NiouiNinoon <83124317+NiouiNinoon@users.noreply.github.com>
2022-06-20 14:44:58 +0200

    Update functions.js

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -3,3 +3,13 @@ $('.m-nav-toggle').click(function(e){
    $('.m-right').toggleClass('is-open')
    $('.m-nav-toggle').toggleClass('is-open')
 })
+
+$('.menu-link').click(function(e){
+   console.log(this);
+   let element = document.getElementById("myHeader");
+   console.log(element);
+   let current = element.getElementsByClassName('current');
+   console.log(current);
+   current.removeClass('current');
+   this.addClass('current');
+})
